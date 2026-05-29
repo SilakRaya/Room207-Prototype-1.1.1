cc4e8d9d8d7306f0e7658b8e115606aec7229962
x40arcade <sobrepenakenneth97@gmail.com>
2026-05-29 21:54:31 +0800

    1.2.6.1
    dialogue switching

--- room-207-prototype/Ep1-TheInBituin/GUI/dialogue.gd
+++ room-207-prototype/Ep1-TheInBituin/GUI/dialogue.gd
@@ -1,32 +1,53 @@
 extends Control
 
-# i am still studying OOP and aplying it hear me out :P -ken
-
 # Encapsulation
+var _Jolina_Lines:Array[String] = [
+	"Dialogue One",
+	"Dialogue Two"
+]
+
 var _full_text:String = ""
 var index:int = 0
+var scene:int = 0
 var typing:bool = false
 var _typing_speed:float = 0.1
 
 @onready var _typing_timer: Timer = $TypingTimer
 
 func _ready() -> void:
-	%Text.visible_characters = 0
-	displayText("Dangal Ng Bayan!")
+	displayText(_Jolina_Lines[scene])
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("switch"):
+		switchText()
 
 # Abstraction
 func displayText(new_text: String) -> void:
 	_full_text = new_text
-	#The length of the text
-	print(_full_text.length())
-	_typing_timer.wait_time = _typing_speed
 	
-	# This starts the timer
-	$TypingTimer.start()
+	# Reset typing
+	index = 0
+	%Text.visible_characters = 0
+	
+	_typing_timer.wait_time = _typing_speed
+	_typing_timer.start()
 	
 	%Text.text = _full_text
 
-# what this signal does if the timer is finished it will increment the index
+func switchText() -> void:
+	scene += 1
+	
+	# Prevent going out of bounds
+	if scene >= _Jolina_Lines.size():
+		scene = 0
+	
+	displayText(_Jolina_Lines[scene])
+
+# Timer signal
 func _on_typing_timer_timeout() -> void:
 	index += 1
 	%Text.visible_characters = index
+	
+	# Stop timer when done
+	if index >= _full_text.length():
+		_typing_timer.stop()
